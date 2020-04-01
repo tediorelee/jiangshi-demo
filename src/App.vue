e75ab42902babc9b808b5e54bafebca97caba467
TEDIORELEE <tediorelee@iidx.xyz>
2020-04-01 19:05:18 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png">
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <jiangShi></jiangShi>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import JiangShi from '@/components/JiangShi.vue'
+
+export default {
+  name: 'app',
+  components: {
+    jiangShi: JiangShi
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/JiangShi.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-card
+    class="box-card"
+    shadow="always"
+  >
+    <!-- <h2>this is a card</h2> -->
+    <el-row>
+      <el-col :span="18">
+        <el-input
+          placeholder="请输入内容"
+          v-model="inputName"
+          clearable
+        >
+        </el-input>
+      </el-col>
+      <el-col :span="6">
+        <el-button
+          type="primary"
+          @click="clickFun"
+        >点我</el-button>
+      </el-col>
+    </el-row>
+  </el-card>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+  name: 'jiangShi'
+})
+export default class JiangShi extends Vue {
+  inputName = ''
+  TextEnum = '我是蒋师'
+
+  created () {
+    // todo
+  }
+
+  clickFun () {
+    this.compareText()
+  }
+
+  compareText () {
+    if (this.inputName === this.TextEnum) {
+      this.$message({
+        message: '恭喜你，这是一条成功消息',
+        type: 'success'
+      })
+    } else {
+      this.$message.error('错了哦，这是一条错误消息')
+    }
+  }
+}
+</script>
+
+<style lang="less">
+.box-card {
+  width: 480px;
+  margin: 0 auto;
+}
+</style>
